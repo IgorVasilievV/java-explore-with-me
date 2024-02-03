91f89bb6914266d72df7d020886f733ec668220a
IgorVasilev <777igorvasiliev@gmail.com>
2024-02-03 15:28:22 +0300

    feat: добавить функциональность комментариев

--- main/src/main/java/ru/practicum/explore/service/comment/ValidateCommentService.java
+++ main/src/main/java/ru/practicum/explore/service/comment/ValidateCommentService.java
@@ -40,8 +40,12 @@ public class ValidateCommentService {
     public void validateBeforeUpdate(Long userId, Long eventId, Long commentId) {
         validateEventId(eventId);
         validateCommentId(commentId);
+        validateUserIdBeforeUpdate(userId, commentId);
+    }
+
+    private void validateUserIdBeforeUpdate(Long userId, Long commentId) {
         validateUserId(userId);
-        if (userId != commentStorage.findById(commentId).get().getInitiator().getId()) {
+        if (!userId.equals(commentStorage.findById(commentId).get().getInitiator().getId())) {
             throw new ValidationException("Comment can't be updated by not initiator.");
         }
     }
@@ -61,10 +65,14 @@ public class ValidateCommentService {
     public void validateBeforeDeleted(Long userId, Long eventId, Long commentId) {
         validateEventId(eventId);
         validateCommentId(commentId);
+        validateUserIdBeforeDelete(userId, commentId);
+    }
+
+    private void validateUserIdBeforeDelete(Long userId, Long commentId) {
         validateUserId(userId);
         Long idOwnerComment = commentStorage.findById(commentId).get().getInitiator().getId();
         Long idOwnerEvent = commentStorage.findById(commentId).get().getEvent().getInitiator().getId();
-        if (userId != idOwnerComment && userId != idOwnerEvent) {
+        if (!userId.equals(idOwnerComment) && !userId.equals(idOwnerEvent)) {
             throw new ValidationException("Comment can't be deleted by not initiator of comment or event.");
         }
     }
